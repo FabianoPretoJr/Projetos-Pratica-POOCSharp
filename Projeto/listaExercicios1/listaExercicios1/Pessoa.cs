6a7219947d86f95076efc5db4fa04c7cd4670f42
FabianoPretoJr <61958796+FabianoPretoJr@users.noreply.github.com>
2020-09-01 09:23:40 -0300

    Novo exercício

---
+++ Projeto/listaExercicios1/listaExercicios1/Pessoa.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listaExercicios1
+{
+    public class Pessoa
+    {
+        private String _nome;
+        public String Nome
+        {
+            get { return _nome; }
+            set { _nome = value; }
+        }
+
+        private DateTime _dataNascimento;
+        public DateTime DataNascimento
+        {
+            get { return _dataNascimento; }
+            set { _dataNascimento = value; }
+        }
+
+        private double _altura;
+        public double Altura
+        {
+            get { return _altura; }
+            set { _altura = value; }
+        }
+
+        private int _idade;
+        public int Idade
+        {
+            get { return _idade; }
+            set { _idade = value; }
+        }
+
+        public void ImprimirDados()
+        {
+            Console.WriteLine("Nome: " + this.Nome);
+            Console.WriteLine("Altura: " + this.Altura);
+            Console.WriteLine("Data de nascimento: " + Convert.ToString(this.DataNascimento).Replace(" 00:00:00", " "));
+            Console.WriteLine("Idade: " + this.CalcularIdade());
+        }
+
+        private int CalcularIdade()
+        {
+            DateTime data = DateTime.Now;
+
+            int dia = data.Day;
+            int mes = data.Month;
+            int ano = data.Year;
+
+            if (this.DataNascimento.Month >= mes)
+            {
+                if (this.DataNascimento.Day <= dia)
+                {
+                    int res = ano - this.DataNascimento.Year;
+                    return res;
+                }      
+                else
+                {
+                    int res = (ano - this.DataNascimento.Year) - 1;
+                    return res;
+                }
+            }
+            else
+            {
+                int res = ano - this.DataNascimento.Year;
+                return res;
+            }
+
+        }
+    }
+}
---
+++ Projeto/listaExercicios1/listaExercicios1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listaExercicios1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pes = new Pessoa();
+
+            Console.Write("Digite seu nome: ");
+            pes.Nome = Console.ReadLine();
+            Console.Write("Digite sua data de nascimento: ");
+            pes.DataNascimento = Convert.ToDateTime(Console.ReadLine());
+            Console.Write("Digite sua altura: ");
+            pes.Altura = Convert.ToDouble(Console.ReadLine());
+
+            pes.ImprimirDados();
+
+            Console.ReadKey();
+        }
+    }
+}
